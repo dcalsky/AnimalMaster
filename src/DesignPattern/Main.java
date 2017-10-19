9247935c0a42a3d9855feed01c51f51b0952461f
Pieres <Pieres@pieres-mac.local>
2017-10-19 18:51:04 +0800

    first

--- src/DesignPattern/Main.java
+++ src/DesignPattern/Main.java
@@ -3,19 +3,12 @@ package DesignPattern;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 	Pig pig1 = Pig.Clone();
 	Pig pig = (Pig)GameObject.findAndClone("DesignPattern.Pig");
 	
 	System.out.println(GameObject.getNumber());
 	System.out.println(Animal.getNumber());
 	// 使用
-=======
-	//Pig pig1 = (Pig) Pig.instance.clone();
-	Pig pig = (Pig)GameObject.findAndClone("DesignPattern.Pig");
-	
-	System.out.println(GameObject.getNumber());
->>>>>>> ddf9484... First
     }
 
 }
