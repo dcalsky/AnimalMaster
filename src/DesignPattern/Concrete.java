a86859c9d1db2a3d20f84f9636b6ae3dbcba2161
Pieres <Pieres@192.168.1.101>
2017-10-19 15:21:10 +0800

    First

---
+++ src/DesignPattern/Concrete.java
@@ -0,0 +1,5 @@
+package DesignPattern;
+
+public interface Concrete {
+    
+}
--- src/DesignPattern/Main.java
+++ src/DesignPattern/Main.java
@@ -3,12 +3,19 @@ package DesignPattern;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
 	Pig pig1 = Pig.Clone();
 	Pig pig = (Pig)GameObject.findAndClone("DesignPattern.Pig");
 	
 	System.out.println(GameObject.getNumber());
 	System.out.println(Animal.getNumber());
 	// 使用
+=======
+	//Pig pig1 = (Pig) Pig.instance.clone();
+	Pig pig = (Pig)GameObject.findAndClone("DesignPattern.Pig");
+	
+	System.out.println(GameObject.getNumber());
+>>>>>>> ddf9484... First
     }
 
 }
