c9b57633f8db58d501a322c3d2b3980253d05413
huanGninGshuanG <1835996411@qq.com>
2017-10-24 11:44:51 +0800

    new action version

--- src/ActionBase/Action.java
+++ src/ActionBase/Action.java
@@ -6,93 +6,88 @@
 package ActionBase;
 
 import Root.GameObject;
-import java.util.HashMap;
-import java.util.Vector;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  *
  * @author 18359
  */
 
-public abstract class Action {
-    protected static HashMap<Action, Vector<GameObject> >  _action = new HashMap<>();
-    protected String actionName;
-    //Vector是一个Observer pool,每一个setAction的动物类会添加到Action对应的Vector当中，Action及其子类使用Prototype设计模式
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    //重载hashcode()与equals()
-    @Override
-    public boolean equals(Object o){
-        return EqualsBuilder.reflectionEquals(this,o);
+public abstract class Action extends GameObject {
+    protected List<Observer> observers = new ArrayList();//observer pool
+    private static List<Action> objs = new ArrayList();
+    protected String _actionName;
+    //Action子类作为subject，每一个setAction的动物作为observer，Action及其子类的实现使用Prototype Design Pattern
+    ////////////////////////////////////////////////////////////////////////////
+    protected Action() { }
+    
+    protected void destroy(Action action){
+	super.destroy(action);
+	objs.remove(action);
+    }
+    //所有继承类需要重载destroy
+    public static int getNumber()
+    {
+	Set<String> set = new HashSet<>();
+	for(Action go : objs)
+	{
+	    set.add(go.getClass().getName());
+	}
+	System.out.println("objs: " +objs.size());
+	System.out.println("set: " + set.size());
+	return objs.size() - set.size();
     }
     
-    @Override
-    public int hashCode(){
-        return HashCodeBuilder.reflectionHashCode(this);
+    protected void addPrototype (Action action) {
+	super.addPrototype(action);
+	objs.add(action);
     }
     
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    //Prototype Design pattern
-    protected static void addPrototype(Action action){ _action.put(action, new Vector()); }
-    protected abstract Action clone();
-    protected abstract void act();
+    ////////////////////////////////////////////////////////////////////////////
+    //getter and setter
+    public String getActionName(){
+        return _actionName;
+    }
     
-    public static Action findAndClone(String type){
-        for(Action action : _action.keySet()){
-            //System.out.println(action.getClass().getName()+" "+_action.keySet().size());
-            if(action.getClass().getName().equals(type)){
-                return action.clone();
-            }
+    public void setActionName(String actionName){
+        _actionName = actionName;
+    }
+    
+    //此函数用于测试
+    public void showContent(){
+        for(Action action:objs){
+            System.out.println(action.getClass().getName());
         }
-        System.out.println(type+" not found");
-        return null;
-    }
-    
-    public static void showContent(){
-        for(Action action: _action.keySet()){
-            //System.out.println(action.getClass().getName());
-            action.act();
+        for(Observer ob:observers){
+            System.out.println(ob.getClass().getName()+" "+this.getClass().getName());
         }
     }
     
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
     //Observer Design pattern
     
-    //向Map中对应的action注册Observer
-    public void attach(Action action, GameObject go){
-            if(_action.containsKey(action)){
-                _action.get(action).add(go);
-                Vector<GameObject> v = _action.get(action);
-                for(GameObject gob: v){
-                    //System.out.println(gob.getClass().getName()+" "+action.getClass().getName());
-                }
-            }else{
-                System.out.println("not exist");
-            }
-            //action1.act();
+    public void attach(Observer ob){
+        observers.add(ob);
     }
 
-    public void dettach(Action action, GameObject go){
-        if(_action.containsKey(action)){
-            if(!_action.get(action).contains(go)){
-                System.out.println(go.getClass().getName()+" does not have action "+action.getClass().getName());
-            }
-            _action.get(action).remove(go);
-            Vector<GameObject> v = _action.get(action);
-                
-            for(GameObject gob: v){
-                //System.out.println(gob.getClass().getName()+" "+action.getClass().getName());
-            }
-        }else{
-            System.out.println("not exist the action");
-        }
-        for(Action action1 : _action.keySet()){
-            System.out.println(action1.getClass().getName());
-            for(GameObject v:_action.get(action1)){
-                System.out.println(v.getClass().getName());
-            }
+    public void detach(Observer ob){
+        observers.remove(ob);
+    }
+    
+    //用户可以对动作进行微调，比如说walk变成walk slower
+    public void changesToAction(String actionName){
+        _actionName = actionName;
+        notifyObserver();
+    }
+    
+    public void notifyObserver(){
+        for(Observer ob: observers){
+            ob.update(_actionName);
         }
     }
+    
     
 }
